786d4b0d1d6d0d4f6682e54916afe5cc8cf19f9d
matthewepler <matthewepler@gmail.com>
2016-11-29 07:50:44 -0500

    sets up file imports and fetches html object

---
+++ scrape.py
@@ -0,0 +1,12 @@
+# Scraper for www.deadlists.com
+# Matthew Epler, 2016 
+# matthewepler@gmail.com
+
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import csv
+
+baseURL = "http://deadlists.com/deadlists/yearresults.asp?KEY=1967"
+html = urlopen(baseURL)
+print(html)
